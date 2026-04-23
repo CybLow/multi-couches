15cc8821f8869c7355f404eaecbcaa84064d43e4
Lois MARTIN <118811498+CybLow@users.noreply.github.com>
2026-04-23 15:57:47 +0200

    feat(entity): scaffold abstract Animal class
    Classe mère vide (abstract) pour la hiérarchie Animal du diagramme UML.
    Les champs et annotations JPA seront ajoutés dans les commits suivants.

---
+++ src/main/java/fr/esaip/petstore/entity/Animal.java
@@ -0,0 +1,10 @@
+package fr.esaip.petstore.entity;
+
+/**
+ * Classe mère abstraite de la hiérarchie des animaux vendus en animalerie.
+ */
+public abstract class Animal {
+
+    protected Animal() {
+    }
+}
